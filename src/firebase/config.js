ae92b349fd58e8686f91a4e24a98a38d392af0b9
miyanokomiya <tomoya.hstexze24632@gmail.com>
2017-08-11 09:19:04 +0900

    firebase設定の初期化

---
+++ src/firebase/config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyCVCGa6K6tgCz5T41ihskjqGB6iOWZvBzU',
+  authDomain: 'sekipon-a4c11.firebaseapp.com',
+  databaseURL: 'https://sekipon-a4c11.firebaseio.com',
+  projectId: 'sekipon-a4c11',
+  storageBucket: 'sekipon-a4c11.appspot.com',
+  messagingSenderId: '832403292755'
+}
